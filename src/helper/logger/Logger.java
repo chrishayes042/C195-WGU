d22c106c09047d6297fe55153de49f4f5c0b1cd8
chris <chrishayes042@gmail.com>
2022-12-10 18:45:28 -0700

    Added logger class

---
+++ src/helper/logger/Logger.java
@@ -0,0 +1,33 @@
+package helper.logger;
+
+import helper.Constants.Constants;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
+public class Logger {
+
+	public static void loginLog(String userName, Boolean success) throws IOException{
+		try{
+			BufferedWriter bw = new BufferedWriter(new FileWriter(Constants.LOGIN_TXT_PATH, true));
+			bw.append(ZonedDateTime.now(ZoneOffset.UTC).toString())
+					.append(Constants.LOGIN_ATTEMPT)
+					.append(userName)
+					.append(Constants.LOGIN_SUCCESS_STRING)
+					.append(success.toString())
+					.append("\n");
+			bw.flush();
+			bw.close();
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+
+	}
+
+
+
+
+}
